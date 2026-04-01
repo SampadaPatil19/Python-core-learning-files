01afa3dd4ff188adaebdf485c505845ceefa61f9
SampadaPatil <150227125+SampadaPatil19@users.noreply.github.com>
2026-04-01 20:10:35 +0530

    Delete assignments directory

--- assignments/1-assignment/question_3.py
+++
@@ -1,8 +0,0 @@
-# 3. Program to find quotient and remainder of two numbers.
-divisor = int(input('Enter the divisor: '))
-dividend = int(input('Enter the dividend: '))
-
-quotient = dividend // divisor
-reminder = dividend % divisor
-
-print(f'quotient is {quotient} and reminder is {reminder}')
--- assignments/2-assignment/question_2.py
+++
@@ -1,7 +0,0 @@
-# 2. Convert temp from Celsius to Fahrenheit. (C/5 = (F-32)/9) 
-
-celsius = float(input('Enter temperature in Celsius: '))
-
-fahrenheit = 9/5 * celsius + 32
-
-print(f'Temperature in Fahrenheit is {fahrenheit}.')
